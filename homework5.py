52e07b9a928be96c65a107aec8d8dd38d6d36091
ZhenyaRom <evladro2020@gmail.com>
2024-06-09 15:18:57 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+immutable_var = (14, 'январь', 2024)
+print(immutable_var)
+#immutable_var[0] = 9
+#immutable_var[1] = 'июнь'
+#print(immutable_var)
+#изменить нельзя, tuple неизменяемый тип данных.
+mytable_list = [14, 'январь', 2024]
+print(mytable_list)
+mytable_list[0] = 9
+mytable_list[1] = 'июнь'
+print(mytable_list)
